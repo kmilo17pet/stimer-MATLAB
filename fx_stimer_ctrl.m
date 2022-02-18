efd7f81a4889ae4c88132e108c4251bd13b3e09b
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2022-02-17 23:28:09 -0500

    minor optimizations

---
+++ fx_stimer_ctrl.m
@@ -0,0 +1,29 @@
+%WARNING Do not call this function directly, instead use stimer().
+%   See also STIMER.
+function status = fx_stimer_ctrl( varargin )
+    persistent xTimer
+
+    if isempty( xTimer )
+        tmp = num2cell( zeros( 1, 256 ) );
+        xTimer = struct( 'st', tmp, 'tv', tmp );
+        tic();
+    end
+
+    index = varargin{ 1 };
+    status = struct( 'expired', false, 'elapsed', 0, 'remaining', 0 );
+    if 1 == nargin 
+        if xTimer( index ).tv > 0
+            status.elapsed = toc( uint64( 0 ) ) - xTimer( index ).st;
+            status.remaining = xTimer( index ).tv - status.elapsed;
+            if status.remaining < 0
+                status.remaining = 0;
+            end
+            status.expired = status.elapsed >= xTimer( index ).tv;
+        end
+    elseif 2 == nargin
+        xTimer( index ).tv = varargin{ 2 };
+        xTimer( index ).st = toc( uint64( 0 ) );
+        status.remaining = xTimer( index ).tv;
+    end
+end
+
--- stimer.m
+++ stimer.m
@@ -16,63 +16,38 @@
 %
 %   Author: J. Camilo Gomez C.
 function status = stimer( varargin )
-    persistent xTimer
-
-    if isempty( xTimer )
-        tmp = num2cell( zeros( 1, 256 ) );
-        xTimer = struct( 'st', tmp, 'tv', tmp );
-        tic();
-    end
-
-    if nargin < 1
+    if nargin < 1 
         error( 'Timer index must be supplied' );
     end
 
-    index = fix( varargin{1} );
-    if any(~isnumeric(index)) || any(isnan(index)) || isempty(index) || any(isinf(index)) || any(~isreal(index))
+    if nargin > 2
+        error( 'Too many input arguments' );
+    end
+    
+    isBadArgument = @( arg ) any( ~isnumeric( arg ) ) || any( isnan( arg ) ) || isempty( arg ) || any( isinf( arg) ) || any( ~isreal( arg ) );
+    
+    varargin{ 1 } = fix( varargin{1} );
+    if isBadArgument( varargin{ 1 } )
         error( 'Index argument not allowed, use only integer values from 1 to 256' );
     end
-    newtime = 0;
 
-    if any( index <= 0 | index > 256 )
+    if any( varargin{ 1 } <= 0 | varargin{ 1 } > 256 )
         error( 'Index exceeds the number of timer instances. Index must not exceed 256' );
     end
 
     if 2 == nargin 
-        newtime = varargin{ 2 };
-        if any( ~isnumeric( newtime  )) || any( isnan( newtime ) ) || isempty( newtime ) || any( isinf( newtime ) ) || any( ~isreal( newtime ) )
+        if isBadArgument( varargin{ 2 } )
             error( 'Time value argument not allowed, use only positive real numbers' );
         end
-        if newtime < 0 
+        if varargin{ 2 } < 0 
             error( 'Time value can not be negative, use only positive real numbers' );
         end        
     end
     
-    if ~isscalar( index ) %operate on multiple requests
-        if 1 == nargin 
-            status = arrayfun( @stimer, index );
-            return;
-        elseif 2 == nargin
-            status = arrayfun( @stimer, index, varargin{ 2 } );
-            return;
-        end
+    if ~isscalar( varargin{ 1 } ) %operate on multiple requests
+        status = arrayfun( @stimer, varargin{:} );
+        return;
     end
 
-    status = struct( 'expired', false, 'elapsed', 0, 'remaining' ,0 );
-    if 1 == nargin 
-        if xTimer( index ).tv > 0
-            status.elapsed = toc( uint64( 0 ) ) - xTimer( index ).st;
-            status.remaining = xTimer( index ).tv - status.elapsed;
-            if status.remaining < 0
-                status.remaining = 0;
-            end
-            status.expired = status.elapsed >= xTimer( index ).tv;
-        end
-    elseif 2 == nargin
-        xTimer( index ).tv = newtime;
-        xTimer( index ).st = toc( uint64( 0 ) );
-        status.remaining = newtime;
-    else
-        error( 'Too many arguments' );
-    end
+    status = fx_stimer_ctrl( varargin{:} );
 end
